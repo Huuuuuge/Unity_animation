15df976d0f8fb059797a0a39b3e54cc306942b85
sapienhomo <105915520+Huuuuuge@users.noreply.github.com>
2023-01-04 21:52:37 +0530

    updated

---
+++ Assets/AnimatorCont.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorCont : MonoBehaviour
+{
+    private Animator myAnim;
+    bool isRunning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isRunning = Input.GetKey(KeyCode.W);
+
+        myAnim.SetBool("run", isRunning);
+    }
+}
